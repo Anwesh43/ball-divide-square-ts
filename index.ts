eea09fe905c56f85e9f06c16a804d08e0057ec12
Anwesh43 <anweshthecool0@gmail.com>
2021-09-03 00:47:19 +0530

    addin BDSNode class

--- index.ts
+++ index.ts
@@ -133,3 +133,45 @@ class Animator {
         }
     }
 }
+
+class BDSNode {
+
+    prev : BDSNode 
+    next : BDSNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BDSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBDSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BDSNode {
+        var curr : BDSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
